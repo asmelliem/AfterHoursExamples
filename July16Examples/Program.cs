561e6e7d5bda28db62222b16acb88ae89ab9b1bf
Ada <asmelliem@gmail.com>
2020-07-16 21:32:27 -0400

    Added July 16th examples for exception handling.

--- July16Examples/Program.cs
+++ July16Examples/Program.cs
@@ -6,7 +6,20 @@ namespace July16Examples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var userInput = Console.ReadLine();
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);                
+            }
+            finally
+            {
+                //For after processing and logic you know won't error out
+                Console.WriteLine("Good job");
+            }
         }
     }
 }

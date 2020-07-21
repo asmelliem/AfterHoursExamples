35d7997c0249041f8c25a169cb874b4313606885
Ada <asmelliem@gmail.com>
2020-07-20 22:04:12 -0400

    Added July 20th examples

---
+++ July20Examples/Class1.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace July20Examples
+{ 
+    public class Class1
+    {
+        [Test]
+        public void OneEqualsOne()
+        {
+            var expected = 1;
+            var actual = 1;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void OneNotEqualTwo()
+        {
+            var expected = 1;
+            var actual = 2;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

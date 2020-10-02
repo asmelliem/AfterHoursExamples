c2e8545d19f7de279504c6b7d5b0652ff366fcde
Ada <asmelliem@gmail.com>
2020-10-01 22:03:35 -0400

    Created project for a calculator MVC app

---
+++ MVC_Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Calculator.Models;
+
+namespace MVC_Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculate()
+        {           
+           //do some work of validating the user input
+           //do some work of actually calculating
+           //display the results of all the above work
+            //display it in the current page?
+            //display it in another page?
+            //do we need another model?
+            //do we need another view? 
+        }
+    }
+}
---
+++ MVC_Calculator/Models/CalculatorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Calculator.Models
+{
+    public class CalculatorViewModel
+    {
+        public decimal InputOne { get; set; }
+
+        public decimal InputTwo { get; set; }
+
+        public string Operation { get; set; }
+
+        public string Result { get; set; }
+    }
+}

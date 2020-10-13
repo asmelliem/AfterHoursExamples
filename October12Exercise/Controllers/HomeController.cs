c72fc8113181e147b80f207c035cbb744c0d460b
Ada <asmelliem@gmail.com>
2020-10-12 21:49:15 -0400

    Added October 12th exercise about sessions and cookies

---
+++ October12Exercise/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using October12Exercise.Models;
+
+namespace October12Exercise.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpContextAccessor _httpContext;
+
+        public HomeController(ILogger<HomeController> logger, IHttpContextAccessor httpContext)
+        {
+            _logger = logger;
+            _httpContext = httpContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult ChangeUI(UISettingModel model)
+        {
+            _httpContext.HttpContext.Session.SetString("font", model.Font);
+            _httpContext.HttpContext.Session.SetString("backgroundColor", model.BackgroundColor);
+
+            ViewData["font"] = model.Font;
+            ViewData["backgroundColor"] = model.BackgroundColor;
+
+            ViewBag.Font = model.Font;
+            ViewBag.BackgroundColor = model.BackgroundColor;
+
+            return View(model);
+        }
+    }
+}
---
+++ October12Exercise/Models/UISettingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace October12Exercise.Models
+{
+    public class UISettingModel
+    {
+        public string Font { get; set; }
+
+        public string BackgroundColor { get; set; }
+    }
+}
